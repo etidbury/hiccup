e886e319633b0727e88f0754d01f844671469947
Edd Large WMG Laptop <tidbury@live.com>
2022-07-28 12:47:54 +0100

    tweaks

--- Dockerfile
+++ Dockerfile
@@ -10,15 +10,40 @@ COPY . .
 
 RUN ["npm","run","build"]
 
-FROM nginx:mainline-alpine as server
+
+
+
+
+
+# FROM bitnami/nginx:latest
+
+# WORKDIR /opt/bitnami/nginx/html
+
+# COPY default.conf /opt/bitnami/nginx/conf/server_blocks/my_server_block.conf
+
+# COPY --from=builder /app/build ./
+
+# COPY entrypoint.sh ./
+
+# ENTRYPOINT [ "/bin/bash" ]
+# CMD ["./entrypoint.sh"]
+
+
+
+FROM nginx:1.13.5
+
 
 WORKDIR /usr/share/nginx/html
 
-COPY default.conf /etc/nginx/conf.d/
+COPY default.conf /etc/nginx/nginx.conf
 
-COPY  --from=builder /app/build ./
+COPY --from=builder /app/build ./
 
 COPY entrypoint.sh ./
 
-ENTRYPOINT [ "/bin/sh" ]
+RUN usermod --non-unique --uid 1000 nginx \
+  && groupmod --non-unique --gid 1000 nginx \
+  && chown -R nginx:nginx /usr/share/nginx/html
+
+ENTRYPOINT [ "/bin/bash" ]
 CMD ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,11 +11,23 @@ if [[ -z "${CONFIG_JSON_REWRITE_DIR}" ]]; then
     exit 1
 fi
 
-cat /etc/nginx/conf.d/default.conf | sed "s|__CUSTOM_PORT__|${CUSTOM_PORT}|" | tee /etc/nginx/conf.d/default.conf > /dev/null
-cat /etc/nginx/conf.d/default.conf | sed "s|__CONFIG_JSON_REWRITE_DIR__|${CONFIG_JSON_REWRITE_DIR}|" | tee /etc/nginx/conf.d/default.conf > /dev/null
+cat /etc/nginx/nginx.conf | sed "s|__CUSTOM_PORT__|${CUSTOM_PORT}|" | tee /etc/nginx/nginx.conf > /dev/null
+cat /etc/nginx/nginx.conf | sed "s|__CONFIG_JSON_REWRITE_DIR__|${CONFIG_JSON_REWRITE_DIR}|" | tee /etc/nginx/nginx.conf > /dev/null
 
-cat /etc/nginx/conf.d/default.conf
+cat /etc/nginx/nginx.conf
 
+
+cat > /usr/share/nginx/html/custom.css <<EON
+#root {
+  background: url(__BACKGROUND_IMAGE_URL__) no-repeat #111 !important;
+  background-position: __BACKGROUND_POSITION__!important;
+}
+EON
+
+cat /usr/share/nginx/html/custom.css | sed "s|__BACKGROUND_POSITION__|${BACKGROUND_POSITION}|" | tee /usr/share/nginx/html/custom.css > /dev/null
+cat /usr/share/nginx/html/custom.css | sed "s|__BACKGROUND_IMAGE_URL__|${BACKGROUND_IMAGE_URL}|" | tee /usr/share/nginx/html/custom.css > /dev/null
+
+ls -la /usr/share/nginx/html
 
 
 nginx -g 'daemon off;'
